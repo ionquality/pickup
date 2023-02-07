65e7d89222df28eb173cf817e4ff0294990aedce
Ian Castillo <ian@ion-connect.com>
2023-02-07 09:56:41 -0800

    -remove migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,33 +15,36 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->integer('op_id')->nullable()->default(null);
-            $table->string('username')->nullable()->default(null);
-            $table->string('first_name')->nullable()->default(null);
-            $table->string('last_name')->nullable()->default(null);
-            $table->string('email')->nullable()->default(null);
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('active','2')->default('Y');
-            $table->string('password');
-            $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('users')){
+            Schema::create('users', function (Blueprint $table) {
+                $table->id();
+                $table->integer('op_id')->nullable()->default(null);
+                $table->string('username')->nullable()->default(null);
+                $table->string('first_name')->nullable()->default(null);
+                $table->string('last_name')->nullable()->default(null);
+                $table->string('email')->nullable()->default(null);
+                $table->timestamp('email_verified_at')->nullable();
+                $table->string('active','2')->default('Y');
+                $table->string('password');
+                $table->rememberToken();
+                $table->foreignId('current_team_id')->nullable();
+                $table->string('profile_photo_path', 2048)->nullable();
+                $table->timestamps();
+            });
 
-        $operators = Security::all();
+            $operators = Security::all();
 
-        foreach ($operators as $operator){
-            User::create([
-                'op_id' => $operator->op_id,
-                'first_name' => trim($operator->first_name),
-                'last_name' => trim($operator->last_name),
-                'username' => $operator->op_passnum,
-                'password' => Hash::make($operator->op_passnum),
-            ]);
+            foreach ($operators as $operator){
+                User::create([
+                    'op_id' => $operator->op_id,
+                    'first_name' => trim($operator->first_name),
+                    'last_name' => trim($operator->last_name),
+                    'username' => $operator->op_passnum,
+                    'password' => Hash::make($operator->op_passnum),
+                ]);
+            }
         }
+
 
     }
 
