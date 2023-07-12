7127e5178f8c521c96a3208b89924f36a59cd3a1
Ian Castillo <ian@ion-connect.com>
2023-07-11 17:43:39 -0700

    -updated users table

---
+++ database/migrations/2023_07_11_174124_update_users_july.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Models\Security;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $operators = Security::all();
+
+        foreach ($operators as $operator){
+            $exist = User::where('op_id', $operator->op_id)->exists();
+            if (!$exist){
+                $name = trim($operator->first_name) . ' ' . trim($operator->last_name);
+                User::create([
+                    'op_id' => $operator->op_id,
+                    'first_name' => trim($operator->first_name),
+                    'last_name' => trim($operator->last_name),
+                    'name' => $name,
+                    'username' => $operator->op_passnum,
+                    'password' => Hash::make($operator->op_passnum),
+                ]);
+            }
+
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
